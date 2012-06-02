808216d20ce641fa2ef422b2f5e38ef2bfea6a60
Jason McMains <jmcmains@gmail.com>
2012-06-02 14:05:51 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+group :development do
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'pg'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PullupLog::Application.config.secret_token = 'e6422a4d89d99be540ce33e972cc2694f70f8f6be643b62b5488e65dc6ef151c7d9e35280eeccf10cd36920ea3bac78b1e84ce6dacf6f64829881aa8b4e96040'
